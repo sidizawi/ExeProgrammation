e9e42b9b69683188f7b4d78947da7adde648eb8c
z-sidi <z.sidi99m@gmail.com>
2019-10-11 15:42:07 +0200

    add sol3_4.py & test_3_4.py

---
+++ Chapitre3/sol3_4.py
@@ -0,0 +1,8 @@
+# Ecrivez juste la fonction parallelogramme()
+
+import math 
+from sol3_3 import longueur
+from sol3_2 import distancePoints
+
+def parallelogramme(*points):
+	pass
---
+++ Chapitre3/test3_4.py
@@ -0,0 +1,9 @@
+import unittest 
+import math
+
+import sol3_4
+
+class ParallelogrammeTest(unittest.TestCase):
+
+	def test_cotes_pas_egaux(self):
+		pass
