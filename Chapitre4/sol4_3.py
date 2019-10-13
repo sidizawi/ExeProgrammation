357d06616e73371c22575462d66579a664ed649c
Sidi M., Zawi <54959353+sidizawi@users.noreply.github.com>
2019-10-13 20:51:59 +0200

    Update sol4_3.py

--- Chapitre4/sol4_3.py
+++ Chapitre4/sol4_3.py
@@ -2,11 +2,4 @@
 # Si il y a pas de bord, la fonction retourne une chaine vide
 
 def plus_grand_bord(w):
-    res = ""
-    sol = []
-    for i in range(int(len(w)/2)+1):
-        if w[0:i+1] == w[-i-1:]:
-            sol.append(w[0:i+1])
-    if sol != []:
-        res = sol[-1]
-    return res
+    pass
